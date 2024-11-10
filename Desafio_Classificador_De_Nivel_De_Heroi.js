7e5da65be9b850d96ea27f28f0d559de20b37782
Carla Cristine Santos Barbosa <100981359+CarlaCristine48@users.noreply.github.com>
2024-11-10 00:10:32 -0300

    Add files via upload

--- Desafio_Classificador_De_Nivel_De_Heroi.js
+++ Desafio_Classificador_De_Nivel_De_Heroi.js
@@ -1,24 +1,24 @@
 // Variáveis para armazenar o nome do herói e a quantidade de experiência (XP) 
 let nomeHeroi = "Homem de ferro"
-let quantidadeXp = 1000
+let quantidadeXp = 1.000
 
 // Variável para armazenar o nível do herói
 let nivelHeroi = ""           
 
 // Estrutura de decisão para determinar o nível com base na quantidade de XP
-if (quantidadeXp <= 1000) {
+if (quantidadeXp <= 1.000) {
     nivelHeroi = "Ferro"
-} else if (quantidadeXp >= 1001 && quantidadeXp <= 2000) {
+} else if (quantidadeXp >= 1.001 && quantidadeXp <= 2.000) {
     nivelHeroi = "Bronze"
-} else if (quantidadeXp >= 2001 && quantidadeXp <= 5000) {
+} else if (quantidadeXp >= 2.001 && quantidadeXp <= 5.000) {
     nivelHeroi = "Prata"
-} else if (quantidadeXp >= 5001 && quantidadeXp <= 7000) {
+} else if (quantidadeXp >= 5.001 && quantidadeXp <= 7.000) {
     nivelHeroi = "Ouro"
-} else if (quantidadeXp >= 7001 && quantidadeXp <= 8000) {
+} else if (quantidadeXp >= 7.001 && quantidadeXp <= 8.000) {
     nivelHeroi = "Platina"
-} else if (quantidadeXp >= 8001 && quantidadeXp <= 9000) {
+} else if (quantidadeXp >= 8.001 && quantidadeXp <= 9.000) {
     nivelHeroi = "Ascendente"
-} else if (quantidadeXp >= 9001 && quantidadeXp <= 10000) {
+} else if (quantidadeXp >= 9.001 && quantidadeXp <= 10.000) {
     nivelHeroi = "Imortal"
 } else {
     nivelHeroi = "Radiante"
